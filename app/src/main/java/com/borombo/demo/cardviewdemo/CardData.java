936e0e6903068188048c4d32a13e69aa6de70719
Borombo <e.rombo@yahoo.com>
2016-03-13 17:26:55 +0100

    Initial Commit - RecycleView avec CardView

---
+++ app/src/main/java/com/borombo/demo/cardviewdemo/CardData.java
@@ -0,0 +1,31 @@
+package com.borombo.demo.cardviewdemo;
+
+/**
+ * Created by Erwan on 13/03/2016.
+ */
+public class CardData {
+
+    private String str1;
+    private String str2;
+
+    public CardData(String str1, String str2) {
+        this.str1 = str1;
+        this.str2 = str2;
+    }
+
+    public String getStr1() {
+        return str1;
+    }
+
+    public void setStr1(String str1) {
+        this.str1 = str1;
+    }
+
+    public String getStr2() {
+        return str2;
+    }
+
+    public void setStr2(String str2) {
+        this.str2 = str2;
+    }
+}
---
+++ app/src/main/java/com/borombo/demo/cardviewdemo/MainActivity.java
@@ -0,0 +1,82 @@
+package com.borombo.demo.cardviewdemo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends ActionBarActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private static String LOG_TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycleView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new RecycleViewAdapter(getDataSet());
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ((RecycleViewAdapter) adapter).setOnItemClickListener(new RecycleViewAdapter
+                .MyClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
+                Log.i(LOG_TAG, " Clicked on Item " + position);
+            }
+        });
+    }
+
+    private ArrayList<CardData> getDataSet(){
+        ArrayList result = new ArrayList<CardData>();
+        for (int i = 0 ; i < 20; i++){
+            CardData data = new CardData("Some primary Text " + i,
+                    "Secondary " + i);
+            result.add(i,data);
+        }
+        return result;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
